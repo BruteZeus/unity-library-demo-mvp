23459ee078cfc85f807167c6a7b5c429e8e9dc8c
Adarsh <itsadarsh8@gmail.com>
2021-04-05 19:02:01 +0530

    first commit

---
+++ Assets/BundledAssets/mynewbundle/LookingScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+public class LookingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public FixedTouchField TouchField;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var fps=GetComponent<RigidbodyFirstPersonController>();
+        fps.mouseLook.LookAxis=TouchField.TouchDist;
+
+    }
+}
